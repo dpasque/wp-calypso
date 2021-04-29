1d3ca744a9438503414a7704207299dae6076379
Bart <bartlomiej.kalisz@gmail.com>
2021-04-29 19:52:09 +0200

    Make sure image upload is based on a proper block id

--- test/e2e/lib/gutenberg/blocks/image-block-component.js
+++ test/e2e/lib/gutenberg/blocks/image-block-component.js
@@ -18,19 +18,17 @@ export class ImageBlockComponent extends GutenbergBlockComponent {
 		 */
 		await driverHelper.clickWhenClickable(
 			this.driver,
-			By.css( 'figure[aria-label="Block: Image"] .components-placeholder__label' )
+			By.css( `${ this.blockID } .components-placeholder__label` )
 		);
-		await driverHelper.waitUntilLocatedAndVisible(
-			this.driver,
-			By.css( '.components-form-file-upload' )
-		);
+
 		const filePathInput = await this.driver.findElement(
-			By.css( '.components-form-file-upload input[type="file"]' )
+			By.css( `${ this.blockID } .components-form-file-upload input[type="file"]` )
 		);
+
 		await filePathInput.sendKeys( fileDetails.file );
-		return await driverHelper.waitTillNotPresent(
+		await driverHelper.waitTillNotPresent(
 			this.driver,
-			By.css( '.wp-block-image .components-spinner' )
+			By.css( `${ this.blockID } .components-spinner` )
 		); // Wait for upload spinner to complete
 	}
 
